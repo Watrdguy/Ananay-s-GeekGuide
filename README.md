d259f2cbf65878acfa223c593a1e46cdaab76153
Watrdguy <ananaysharma299@gmail.com>
2025-10-17 23:46:49 +0530

    Setting Up

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple computer price predictor with react + tailwind frontend and flask backend
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+      Hello World!
+      </div>
+    </>
+  )
+}
+
+export default App
